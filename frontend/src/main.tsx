f3c63d60741dbbfbffc09536969a1170abebcbaa
miloe2 <miloe0117@gmail.com>
2025-04-21 10:52:36 +0900

    [Chore] react-query-dev 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -17,5 +17,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    {import.meta.env.DEV && <ReactQueryDevtools />}
   </QueryClientProvider>
 );
