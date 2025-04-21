abbf3271295cbf7a6c6989939861848a0678143f
miloe2 <miloe0117@gmail.com>
2025-04-21 10:03:31 +0900

    [Chore] React-query install

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -6,12 +6,12 @@ const BASE_URL = "http://172.20.10.3:5000";
 
 
 // 모든 데이터 가져오기
-export const fetchData: ApiTypes["fetchData"] = async ({table, year, month}) => {
+export const fetchData: ApiTypes["fetchData"] = async ({ table, year, month }) => {
   console.log("fetchData", table, "api start######");
   try {
     const rsp = await axios.get(`${BASE_URL}/api/sales`, {
       params: {
-        category : table,
+        category: table,
         year,
         month,
       },
@@ -22,7 +22,6 @@ export const fetchData: ApiTypes["fetchData"] = async ({table, year, month}) => 
     console.error("오류 발생!", error);
   }
 };
-
 
 // 모든 통계 가져오기
 export const fetchTotalData: ApiTypes["fetchTotalData"] = async () => {
@@ -36,21 +35,6 @@ export const fetchTotalData: ApiTypes["fetchTotalData"] = async () => {
   }
 };
 
-
-/** process
-- 사용자입력
-- pendignChanges { id, key, value } 저장
-- 기존 id, key가 동일하다면, 가장 최근에 들어온 입력값만 저장 
--> interval 5초마다 updateData(pendingChanges)호출 
-### updateData(pendingChanges) 설명
-- pengdingChanges.length !== 0
-- pendingChanges를 id값으로 group화
-- group.foreach(id값에 따른 api 호출 (생성/수정))
-- (생성Postapi 호출 시) tempId를 Map저장
-- 생성Post api 200일 때, real_id와 를 Map에 value로 저장
-- zustand tableData의 temp 아이디를 real_id로 변환 
-- pendingChange = [] 초기화
-*/
 
 // 데이터 추가하기
 export const fetchAddData: ApiTypes["fetchAddData"] = async (newData) => {
@@ -84,3 +68,8 @@ export const fetchUpdateData: ApiTypes["fetchUpdateData"] = async (rawData) => {
     throw error;
   }
 };
+
+/*
+리액트 쿼리로 겟 요청 캐싱
+배포
+*/
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,21 @@
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
+// main.tsx
 import './index.css'
 import App from './App.tsx'
-// main.tsx
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
